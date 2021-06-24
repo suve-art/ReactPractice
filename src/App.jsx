89061d4f0ed770911ee879d32c8d888184e3fad0
suve-art <suvethasuve352@gmail.com>
2021-06-24 20:05:48 +0530

    24 june

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,113 @@
-import React, { useState } from "react";
-import "./App.scss";
-import Webicon from "./assets/image.svg";
-import Arrowleft from "./assets/arrowleft.svg";
-import Arrowright from "./assets/arrowright.svg";
-import Blurleft from "./assets/leftblur.svg";
-import Blurright from "./assets/blurright.svg";
+import React, { useState } from 'react';
+import './App.scss';
+import Webicon from './assets/image.svg';
+import Arrowleft from './assets/arrowleft.svg';
+import Arrowright from './assets/arrowright.svg';
+import Blurleft from './assets/leftblur.svg';
+import Blurright from './assets/blurright.svg';
 
 function App() {
-  const [screen, setScreen] = useState("usedWebsites");
+  const [screen, setScreen] = useState('usedWebsites');
   const changeScreen = (screenName) => {
     setScreen(screenName);
   };
   return (
-    <div className="App">
-      <div className="btn_container">
-        <button class="btn">Today</button>
-        <button class="btn active">Last 30 days</button>
+    <div className='App'>
+      <div className='view_time_wrapper'>
+
+        <div className='radio_button_container'>
+          <input type='radio' name='view' id='view_today' className='no_display' />
+          <label className='radio_click' htmlFor='view_today'>Today</label>
+        </div>
+
+
+        <div className='radio_button_container'>
+          <input type='radio' name='view' id='view_30_days' className='no_display' />
+          <label className='radio_click' htmlFor='view_30_days'>Last 30 days</label>
+        </div>
+
       </div>
-      <div className="container">
-          <div className="box"></div>
-        <div className="box">
-          {screen === "usedWebsites" && (
-            
-              <>
-                <div className="head">
-                  <h1>Most Used Websites</h1>
-                  <div className="side_btn">
-                    <img src={Blurleft} ></img>
-                    <img src={Arrowright} onClick={()=>changeScreen('socialMedia')}></img>
-                  </div>
-                </div>
-                <div className="content">
-                  <ul>
-                    <li className="first_row">
-                      <img src={Webicon}></img>
-                      <span className="text">gmail.com</span>
-                      <span className="time">1h 32m</span>
-                    </li>
-                    <li className="first_row">
-                      <img src={Webicon}></img>
-                      <span className="text">youtube.com</span>
-                      <span className="time">44m</span>
-                    </li>
-                    <li className="first_row">
-                      <img src={Webicon}></img>
-                      <span className="text">bbc.com</span>
-                      <span className="time">56m</span>
-                    </li>
-                    <li className="first_row">
-                      <img src={Webicon}></img>
-                      <span className="text">youtube.com</span>
-                      <span className="time">44m</span>
-                    </li>
-                    <li className="first_row">
-                      <img src={Webicon}></img>
-                      <span className="text">bbc.com</span>
-                      <span className="time">56m</span>
-                    </li>
-                  </ul>
-                </div>
-              </>
-          )}
-          { screen === 'socialMedia' &&
+      <div className='screen_container'>
+        <div className='screen'></div>
+        <div className='screen'>
+          {screen === 'usedWebsites' &&
             <>
-              <div className="head">
-                <h1>Most Used Social Apps</h1>
-                <div className="side_btn">
-                  <img src={Arrowleft} onClick={()=>changeScreen('usedWebsites')}></img>
-                  <img src={Blurright}></img>
+              <div className='page_title'>
+                <h1>Most Used Websites</h1>
+                <div className='page_next'>
+
+                  <span className='websites_arrow_right' ></span>
+                  <span className='websites_arrow_left' onClick={() => changeScreen('socialMedia')}></span>
                 </div>
               </div>
-              <div className="content">
+              <div className='screen_content'>
                 <ul>
-                  <li className="first_row">
-                    <div className="picture">f</div>
-                    <span className="text">Facebook</span>
-                    <span className="time">1h 32m</span>
+                  <li className='website_list'>
+
+                    <span className='website_icon' ></span>
+                    <span className='website_name'>gmail.com</span>
+                    <span className='time_spent'>1h 32m</span>
                   </li>
-                  <li className="first_row">
-                    <div className="picture">t</div>
-                    <span className="text">Twitter</span>
-                    <span className="time">45m</span>
+                  <li className='website_list'>
+                    <span className='website_icon' ></span>
+                    <span className='website_name'>youtube.com</span>
+                    <span className='time_spent'>44m</span>
                   </li>
-                  <li className="first_row">
-                    <div className="picture">s</div>
-                    <span className="text">Snapchat</span>
-                    <span className="time">56m</span>
+                  <li className='website_list'>
+                    <span className='website_icon' ></span>
+                    <span className='website_name'>bbc.com</span>
+                    <span className='time_spent'>56m</span>
                   </li>
-                  <li className="first_row">
-                    <div className="picture">t</div>
-                    <span className="text">Tik Tok</span>
-                    <span className="time">2h 32m</span>
+                  <li className='website_list'>
+                    <span className='website_icon' ></span>
+                    <span className='website_name'>youtube.com</span>
+                    <span className='time_spent'>44m</span>
                   </li>
-                  <li className="first_row">
-                    <div className="picture">i</div>
-                    <span className="text">Instagram</span>
-                    <span className="time">34m</span>
+                  <li className='website_list'>
+                    <span className='website_icon' ></span>
+                    <span className='website_name'>bbc.com</span>
+                    <span className='time_spent'>56m</span>
+                  </li>
+                </ul>
+              </div>
+            </>
+          }
+          {screen === 'socialMedia' &&
+            <>
+              <div className='page_title'>
+                <h1>Most Used Social Apps</h1>
+                <div className='page_next'>
+
+                  <span className='social_arrow_right' onClick={() => changeScreen('usedWebsites')}></span>
+                  <span className='social_arrow_left' ></span>
+                </div>
+              </div>
+              <div className='screen_content'>
+                <ul>
+                  <li className='website_list'>
+                    <div className='app_logo'>f</div>
+                    <span className='website_name'>Facebook</span>
+                    <span className='time_spent'>1h 32m</span>
+                  </li>
+                  <li className='website_list'>
+                    <div className='app_logo'>t</div>
+                    <span className='website_name'>Twitter</span>
+                    <span className='time_spent'>45m</span>
+                  </li>
+                  <li className='website_list'>
+                    <div className='app_logo'>s</div>
+                    <span className='website_name'>Snapchat</span>
+                    <span className='time_spent'>56m</span>
+                  </li>
+                  <li className='website_list'>
+                    <div className='app_logo'>t</div>
+                    <span className='website_name'>Tik Tok</span>
+                    <span className='time_spent'>2h 32m</span>
+                  </li>
+                  <li className='website_list'>
+                    <div className='app_logo'>i</div>
+                    <span className='website_name'>Instagram</span>
+                    <span className='time_spent'>34m</span>
                   </li>
                 </ul>
               </div>
